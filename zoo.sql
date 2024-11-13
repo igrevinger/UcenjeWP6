d34d13f317952b9c9b88fbb30a7589d0b0cd94d5
igrevinger <grevinger.ivan@gmail.com>
2024-11-13 18:14:26 +0100

    kreiranje tablica

--- zoo.sql
+++ zoo.sql
@@ -21,18 +21,32 @@ go
 
 --use zoo;
 
+create table djelatnik(
+sifra int not null primary key identity(1,1),
+ime varchar(50) not null,
+prezime varchar(50) not null,
+IBAN varchar(50)
+);
+
+create table prostorija(
+sifra int not null primary key identity(1,1),
+dimanzije varchar(30) not null,
+maks_broj int not null,
+mjesto varchar(30)
+);
+
+create table datum(
+d_rodenja datetime,
+d_dolaska datetime,
+d_smrti datetime,
+sifra int not null primary key identity(1,1)
+);
+
 create table zivotinja(
 sifra int not null primary key identity(1,1),
 vrsta varchar(50) not null,
 ime varchar(50) not null,
 djelatnik int not null,
 prostorija int not null,
-datum int not null
-);
-
-create table djelatnik(
-sifra int not null primary key identity(1,1),
-ime varchar(50) not null,
-prezime varchar(50) not null,
-IBAN varchar(50)
+datum int not null references datum(sifra)
 );
