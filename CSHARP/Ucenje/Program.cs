87a0959b58b1a72ff4b4f73f9a4ae427ba6ead2c
igrevinger <grevinger.ivan@gmail.com>
2024-12-02 17:51:25 +0100

    Upoznavanje sa sintaksom

--- CSHARP/Ucenje/Program.cs
+++ CSHARP/Ucenje/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//komentar jedna linija
+/*
+Komentar više linija
+*/
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Pozdrav Svijetu ČŠĆĐŽ čšćđž");
+Console.WriteLine("Hello Osijek");
