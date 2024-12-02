cf75455e2feba70471672de87d3e4e753fd5b46c
igrevinger <grevinger.ivan@gmail.com>
2024-12-02 18:51:30 +0100

    Ulaz Izlaz

--- CSHARP/Ucenje/Program.cs
+++ CSHARP/Ucenje/Program.cs
@@ -1,9 +1,12 @@
-﻿
+﻿using Ucenje;
+
+E01UlazIzlaz.Izvedi();
+
 //komentar jedna linija
 /*
 Komentar više linija
 */
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Pozdrav Svijetu ČŠĆĐŽ čšćđž");
-Console.WriteLine("Hello Osijek");
+//Console.WriteLine("Pozdrav Svijetu ČŠĆĐŽ čšćđž");
+//Console.WriteLine("Hello Osijek");
