735c76f59acdc1a06318e76f4b1d5609da6b6036
igrevinger <grevinger.ivan@gmail.com>
2025-01-07 21:31:22 +0100

    Izjednačavanje

--- CSHARP/Ucenje/E01UlazIzlaz.cs
+++ CSHARP/Ucenje/E01UlazIzlaz.cs
@@ -1,27 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ucenje
+﻿namespace Ucenje
 {
     internal class E01UlazIzlaz
     {
 
         public static void Izvedi()
         {
+            // Ovo je izlaz
             Console.Write("Unesi svoje ime: ");
 
-            //Ulaz podataka u program
+
+            // Ulaz podataka u program
             string Ime = Console.ReadLine();
 
             Console.WriteLine("Unijeli ste " + Ime);
 
-            //formatirani način ispisa
+            // formatirani način ispisa
             Console.WriteLine("Unijeli ste {0}. Bravo!", Ime);
+
         }
- 
 
 
     }
